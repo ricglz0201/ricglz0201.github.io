481cb14054007730ce37f8c004c5e49e6ef0b53a
Ricardo González Castillo <rjgcastillo@hotmail.com>
2019-10-28 19:25:13 -0600

    Move App.js to components and implement a method for changing the theme when a button is clicked

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+// @flow strict-local
+import 'typeface-roboto';
+import WbSunnyIcon  from '@material-ui/icons/WbSunny';
+import IconButton from '@material-ui/core/IconButton';
+import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { lightTheme, darkTheme } from '../helpers/themes';
+
+function App() {
+  const [theme, setTheme] = React.useState(lightTheme);
+  const changeTheme = React.useCallback(() => {
+    setTheme(prevTheme => {
+      if(prevTheme === lightTheme) return darkTheme;
+
+      return lightTheme;
+    });
+  }, []);
+  return (
+    <ThemeProvider theme={theme}>
+      <IconButton color="primary" onClick={changeTheme}>
+        <WbSunnyIcon />
+      </IconButton>
+    </ThemeProvider>
+  );
+}
+
+export default App;
