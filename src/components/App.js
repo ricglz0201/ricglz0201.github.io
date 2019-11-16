5915562fc20001fd0e57d5f67661b974d5f67422
Ricardo González Castillo <rjgcastillo@hotmail.com>
2019-11-15 22:19:45 -0600

    Create useTheme custom hook and keep the previously chosen theme with local storage

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,10 @@ import Navbar from './Navbar';
 import Content from './Content';
 import Wrapper from './Wrapper';
 import { ThemeProvider } from '@material-ui/core/styles';
-import { lightTheme, darkTheme } from '../helpers/themes';
+import { useTheme } from '../helpers/themes';
 
 function App(): React.Node {
-  const [theme, setTheme] = React.useState(lightTheme);
-  const changeTheme = React.useCallback(() => {
-    setTheme(prevTheme => {
-      if(prevTheme === lightTheme) return darkTheme;
-
-      return lightTheme;
-    });
-  }, []);
+  const [theme, changeTheme] = useTheme();
   return (
     <ThemeProvider theme={theme}>
       <Wrapper>
