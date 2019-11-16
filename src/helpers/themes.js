68cdadc01906b2d1fe98ed408350025b9f958770
Ricardo González Castillo <rjgcastillo@hotmail.com>
2019-11-15 22:19:45 -0600

    Create useTheme custom hook and keep the previously chosen theme with local storage

--- src/helpers/themes.js
+++ src/helpers/themes.js
@@ -1,7 +1,9 @@
 // @flow
-import lightBlue from '@material-ui/core/colors/lightBlue';
 import amber from '@material-ui/core/colors/amber';
+import lightBlue from '@material-ui/core/colors/lightBlue';
 import { createMuiTheme, Theme } from '@material-ui/core/styles';
+import { useState, useCallback } from 'react';
+import { useLocalStorage } from './custom_hooks';
 
 export const lightTheme: Theme = createMuiTheme({
   palette: {
@@ -16,3 +18,16 @@ export const darkTheme: Theme = createMuiTheme({
     primary: amber,
   }
 });
+
+type useThemeReturnType = [Theme, () => void];
+
+export const useTheme = () : useThemeReturnType => {
+  const [storedValue, setStoredValue] = useLocalStorage('theme', 'light');
+  const preferredTheme = storedValue === 'light' ? lightTheme : darkTheme;
+  const [theme, setTheme] = useState(preferredTheme);
+  const changeTheme = useCallback(() => {
+    setTheme(prevTheme => prevTheme === lightTheme ? darkTheme : lightTheme);
+    setStoredValue( prevValue => prevValue === 'light' ? 'dark' : 'light' );
+  }, [setStoredValue]);
+  return [theme, changeTheme];
+}
