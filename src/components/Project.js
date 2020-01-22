ebbd386cffa009f0aecdf83acabe2e61cab35233
Ricardo González Castillo <rjgcastillo@hotmail.com>
2020-01-22 16:13:18 -0600

    Remove repition of code between content

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,41 +1,22 @@
 // @flow
 import * as React from 'react';
 import Box from '@material-ui/core/Box';
-import CustomTypography from './CustomTypography';
 import Grid from '@material-ui/core/Grid';
 import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
-import projectData from './projectData';
-import { createStyles, makeStyles } from '@material-ui/core/styles';
-
-type Style = {
-  root: {
-    padding: string,
-    height: string,
-  },
-};
 
 type AchievementsProps = {|
   achievements: Array<string>,
 |}
 
-type ProjectProps = {|
+export type ProjectProps = {|
   achievements: Array<string>,
-  website: string,
   description: string,
   name: string,
   period: string,
   technologies: string,
+  website: string,
 |};
-
-const useStyles = makeStyles(() : Style =>
-  createStyles({
-    root: {
-      padding: '1rem',
-      height: '100%',
-    }
-  })
-);
 
 const Achievements = ({ achievements }: AchievementsProps) => (
   <ul>
@@ -69,21 +50,4 @@ const Project = ({
   </Grid>
 )
 
-const ProjectsContent = () : React.Node => {
-  const classes = useStyles();
-  return (
-    <Grid container spacing={3} className={classes.root}>
-      <CustomTypography textAlign="center" fontSize="h5">
-        Projects
-      </CustomTypography>
-      {
-        projectData.map(
-          (experience, index) => <Project key={index} {...experience} />
-        )
-      }
-    </Grid>
-  )
-};
-
-export default ProjectsContent;
-
+export default Project;
